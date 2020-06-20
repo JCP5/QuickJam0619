c772655d1d0049a635308edd6ddae87e7548e3bb
Nick.Thiessen <nicolas.thiessen@gmail.com>
2020-06-20 01:00:50 -0500

    Initial tutorial work

---
+++ QuickJam0619/Assets/LevelAssets/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject enemyToSpawn;
+
+    private float spawnRate;
+
+    public Transform[] spawnSpots;
+
+    private float timeBetweenSpawns;
+    public float startTimeBetweenSpawns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetweenSpawns = startTimeBetweenSpawns;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBetweenSpawns <= 0)
+        {
+            Instantiate(enemyToSpawn, spawnSpots[Random.Range(0, spawnSpots.Length - 1)].position, Quaternion.identity);
+            timeBetweenSpawns = startTimeBetweenSpawns;
+        }
+        else
+        {
+            timeBetweenSpawns -= Time.deltaTime;
+        }
+    }
+}
